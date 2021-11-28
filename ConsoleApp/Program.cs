8fb5f8a663a0613a3d29627efade9c41a1da6895
newvlad2001 <newvlad2001@gmail.com>
2021-11-28 18:12:22 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.TestGenerator.Implementations;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new NUnitTestGenerator(10, 10, 10);
+            generator.GenerateTestsAsync(@"D:\Projects\StudioProjects\C# projects\mpp-lab4\ConsoleApp\input",
+                @"D:\Projects\StudioProjects\C# projects\mpp-lab4\ConsoleApp\output").Wait();
+        }
+    }
+}
